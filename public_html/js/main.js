ba733151d3cc827bfbf6b9fa303986b18bd635d0
Rafael Passos Guimarães <rapassos@gmail.com>
2020-07-21 03:08:14 -0300

    Aula 1 - Criação do HTML básico e vinculação CSS e Javascript - Criação de variáveis e comandos básicos de Javascript

---
+++ public_html/js/main.js
@@ -0,0 +1,19 @@
+
+var nome = "Rafael Passos";
+var idade = 38;
+//alert("Bem vindo " + nome);
+console.log("Bem vindo " + nome);
+//alert("Sua idade é " + idade + " anos");
+console.log("Sua idade é " + idade + " anos");
+
+
+var valor1 = idade;
+var valor2 = 10;
+//alert(valor1 + " + " + valor2 + " = "+ (valor1+valor2));
+console.log(valor1 + " + " + valor2 + " = "+ (valor1+valor2));
+
+var frase = "O Japão é o melhor time do mundo!!!";
+console.log(frase);
+console.log(frase.replace("Japão","Brasil"));
+frase = frase.replace("Japão","Brasil");
+console.log(frase.toLocaleUpperCase());
